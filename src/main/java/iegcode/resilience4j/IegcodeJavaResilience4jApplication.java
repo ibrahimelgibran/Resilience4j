21e4f6ec9449f2b935c3585895a5c09acf21097b
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-01-31 21:02:12 +0700

    first commit

---
+++ src/main/java/iegcode/resilience4j/IegcodeJavaResilience4jApplication.java
@@ -0,0 +1,13 @@
+package iegcode.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IegcodeJavaResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IegcodeJavaResilience4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iegcode/resilience4j/IegcodeJavaResilience4jApplicationTests.java
@@ -0,0 +1,13 @@
+package iegcode.resilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IegcodeJavaResilience4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
