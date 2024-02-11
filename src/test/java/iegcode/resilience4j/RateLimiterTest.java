d66c7f28212c086418b09824541aa2981173508a
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-02-11 11:23:30 +0700

    update

--- src/test/java/iegcode/resilience4j/RateLimiterTest.java
+++ src/test/java/iegcode/resilience4j/RateLimiterTest.java
@@ -1,9 +1,12 @@
 package iegcode.resilience4j;
 
 import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.util.Deque;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
@@ -16,6 +19,26 @@ public class RateLimiterTest {
         RateLimiter rateLimiter = RateLimiter.ofDefaults("IEG");
 
         for (int i = 0; i < 100; i++) {
+            Runnable runnable = RateLimiter.decorateRunnable(rateLimiter, () -> {
+                long result = counter.incrementAndGet();
+                log.info("Result: {}", result);
+            });
+            runnable.run();
+        }
+
+    }
+
+    @Test
+    void testRateLimiterConfig() {
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitForPeriod(100)
+                .limitRefreshPeriod(Duration.ofMinutes(1))
+                .timeoutDuration(Duration.ofSeconds(2))
+                .build();
+
+        RateLimiter rateLimiter = RateLimiter.of("IEG", config);
+
+        for (int i = 0; i < 10_000; i++) {
             Runnable runnable = RateLimiter.decorateRunnable(rateLimiter, () -> {
                 long result = counter.incrementAndGet();
                 log.info("Result: {}", result);
